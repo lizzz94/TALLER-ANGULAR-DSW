1ad0ef6b744123673123984724863d9aeccf9a42
Lizeth Bejarano <l.bejarano1@uniandes.edu.co>
2025-04-20 22:42:23 -0500

    little update

--- series/src/app/serie/serie-list/serie-list.component.ts
+++ series/src/app/serie/serie-list/serie-list.component.ts
@@ -9,13 +9,28 @@ import { SerieService } from '../serie.service';
 })
 export class SerieListComponent implements OnInit {
   series: Serie[] = [];
+  selectedSerie?: Serie;
+  averageSeasons: number = 0;
 
   constructor(private serieService: SerieService) {}
 
   ngOnInit(): void {
+    this.getSeries();
+  }
+
+  getSeries(): void {
     this.serieService.getSeries().subscribe(series => {
       this.series = series;
+      this.calculateAverage();
     });
   }
-}
 
+  onSelect(serie: Serie): void {
+    this.selectedSerie = serie;
+  }
+
+  calculateAverage(): void {
+    const totalSeasons = this.series.reduce((acc, serie) => acc + serie.seasons, 0);
+    this.averageSeasons = totalSeasons / this.series.length;
+  }
+}
